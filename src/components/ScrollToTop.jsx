328334485499dfa8f18fdf78a8cb41f9be3acc84
marlaw21 <marlaw43@comcast.net>
2025-04-30 22:54:00 +0000

    fin

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,26 +1,17 @@
-import { useEffect, useRef } from "react";
-import PropTypes from "prop-types";
+import React from "react";
+import { Link } from "react-router-dom";
 
-// This component allows the scroll to go to the beginning when changing the view,
-// otherwise it would remain in the position of the previous view. 
-// Investigate more about this React behavior :D 
-
-const ScrollToTop = ({ location, children }) => {
-    const prevLocation = useRef(location);
-
-    useEffect(() => {
-        if (location !== prevLocation.current) {
-            window.scrollTo(0, 0);
-        }
-        prevLocation.current = location;
-    }, [location]);
-
-    return children;
-};
-
-export default ScrollToTop;
-
-ScrollToTop.propTypes = {
-    location: PropTypes.object,
-    children: PropTypes.any
+export const Navbar = () => {
+	return (
+		<nav className="navbar navbar-light bg-light mb-3">
+			<Link to="/">
+				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+			</Link>
+			<div className="ml-auto">
+				<Link to="/demo">
+					<button className="btn btn-primary">Check the Context in action</button>
+				</Link>
+			</div>
+		</nav>
+	);
 };
